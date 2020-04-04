df0becae335bfa73053282db40a447a5bcde3306
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-04 11:27:50 +0900

    change_routes
    rootをrestaurantsのindexへ変更
    
    indexがトップページになるため。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "restaurants#index"
 end
