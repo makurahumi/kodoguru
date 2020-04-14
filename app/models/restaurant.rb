a5eeb7480ff664a39a59fe15978b420a4e19b732
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-14 18:32:01 +0900

    add_image_association
    imageモデルとrestaurantモデルにアソシエーションを記述

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,8 @@ class Restaurant < ApplicationRecord
     belongs_to_active_hash :cuisine
 
   belongs_to :user
+  has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, presence: true
   validates :cuisine_id, presence: true
@@ -11,5 +13,8 @@ class Restaurant < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :city, presence: true
 
-  enum form_id: { '店内飲食可': 1, '店内飲食可・テイクアウトあり': 2, '店内飲食可・テイクアウトあり・デリバリーあり': 3, 'テイクアウトのみ': 4, 'デリバリーのみ': 5, 'テイクアウト・デリバリーのみ': 6, '店頭販売のみ': 7 }
+  enum form_id: { '店内飲食可': 1, '店内飲食可・テイクアウトあり': 2, 
+                  '店内飲食可・テイクアウトあり・デリバリーあり': 3, 
+                  'テイクアウトのみ': 4, 'デリバリーのみ': 5, 
+                  'テイクアウト・デリバリーのみ': 6, '店頭販売のみ': 7 }
 end
