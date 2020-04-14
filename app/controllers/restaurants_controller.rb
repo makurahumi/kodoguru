8059cf927afa03c49b069aac0eae2cd804aeac2b
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-14 22:22:08 +0900

    add_new_action
    newアクション、createアクションを記述

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,5 +4,22 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+    @restaurant.images.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :detail, :cuisine_id, :form_id, :holiday, :accsess, :prefecture_id, :city, :block, :building_name, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+    end 
 end
