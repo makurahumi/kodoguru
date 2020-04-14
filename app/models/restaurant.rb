c67bc0e56b634a022dcd666f11d8799a7ecb36d7
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-14 11:05:50 +0900

    add_prefecture
    prefctureモデルを追加
    
    ・gem active_hashを利用し、都道府県データを追加
    ・restaurantモデルにアソシエーションを追加
    ・viewに適応
    ・readmeを修正
    ・userモデルにアソシエーションを追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  belongs_to :user
+
+  vaildates :name, presence: true
+  validates :cuisine_id, presence: true
+  validates :form_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
 end
