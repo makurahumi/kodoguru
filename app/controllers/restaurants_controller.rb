f70f40436c323657a55be8616a1af329b04157aa
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-16 22:51:44 +0900

    add_update
    restaurantコントローラーにupdateアクションを追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only:[:show, :edit, :update]
 
   def index
   end
@@ -18,11 +19,17 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -30,4 +37,8 @@ class RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :detail, :cuisine_id, :form_id, :holiday, :open, :closed, :accsess, :prefecture_id, :city, :block, :building_name, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
     end 
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
 end
