a2e65e1d3c4131bffa06ed98cf0bffb8db3964ae
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-17 22:15:20 +0900

    add_dependent_destroy
    imagesモデルアソシエーションにdeprndent::destroyを追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,7 @@ class Restaurant < ApplicationRecord
     belongs_to_active_hash :cuisine
 
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, presence: true
