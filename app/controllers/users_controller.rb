badbb17f2a15b0bc685d1793e65faca91377b9ef
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-18 15:06:39 +0900

    change_path_method
    TOPページのマイページリンクパスを変更
    
    ・マイページリンクをuser_pathとcurrent_user.idに変更。
    ・users_controllerにshowアクションの内容記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only:[:index]
+  before_action :set_user, only:[:show]
 
-  def index
+  def show
+    @restaurants = Restaurant.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
   end
 
   private
