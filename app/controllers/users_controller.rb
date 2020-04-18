78b8ee90acd6a7151a00f9e1b6a20c54fc426948
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-18 19:17:37 +0900

    add_update_method
    updateアクションの内容を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,20 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
    def set_user
     @user = current_user
+   end
+
+   def user_params
+    params.require(:user).permit(:nickname)
    end
 end
