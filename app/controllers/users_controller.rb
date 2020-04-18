b192663097a250cadcbe8cc68478b4e8c67663a4
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-18 18:18:46 +0900

    add_controller_action
    usersコントローラーにeditとupdateアクションを記述。before_actionに追加。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only:[:show]
+  before_action :set_user, only:[:show, :edit, :update]
 
   def show
     @restaurants = Restaurant.where(user_id: current_user.id).page(params[:page]).per(3).order("created_at DESC")
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
