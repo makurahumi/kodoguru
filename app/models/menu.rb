3383f41adb2f9aef48bf0fe6e6de7308ef45dc45
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-19 12:17:32 +0900

    add_model_imageuploader
    menuモデルにimageuploaderを追記

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,8 @@ class Menu < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
+  mount_uploader :photo, ImageUploader
+
   validates :menuname, presence: true
   validates :price, presence: true
 end
