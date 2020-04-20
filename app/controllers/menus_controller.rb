434fb8bff9016f45cf20d047da522ed9141f893f
makurahumi <ethelandce.01kannna@gmail.com>
2020-04-20 15:33:46 +0900

    change_controller
    各種アクション記述

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,22 @@
 class MenusController < ApplicationController
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @menu = Menu.new
   end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def menu_params
+      params.require(:menu).permit(:menuname, :photo, :price, :detail).merge(user_id: current_user.id, restaurant_id: params[:restaurant_id])
+    end
 end
